e41ed765bf8fd21c8b32a9ff45c6528b3d7efba8
Andrew Lee <me@gilgameshskytrooper.io>
2019-01-08 12:44:10 -0600

    update.sh -> release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 commit=$(git log -1 --pretty=%B | head -n 1)
-version=$(echo $(curl -s https://api.github.com/repos/voiceittech/VoiceItApi2AndroidSDK/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/') | tr "." "\n")
+version=$(echo $(curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -s https://api.github.com/repos/voiceittech/VoiceItApi2AndroidSDK/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/') | tr "." "\n")
 set -- $version
 major=$1
 minor=$2
